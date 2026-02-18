ded1c27057a44c52829684b4b2380cfb776465f8
Calamity-Kitten <Calamity-Kitten@protonmail.com>
2026-02-17 22:46:17 -0500

    Add recurring game update check and ability to change mode

--- main/game.cpp
+++ main/game.cpp
@@ -2,8 +2,19 @@
 
 static unsigned char gameMode = GAME_MODE_OFF;
 
+void changeGameMode(unsigned char newGameMode) {
+  if (gameMode == newGameMode) {
+    // TODO: Should this reset the game?
+    return;
+  }
+
+  gameMode = newGameMode;
+}
+
 void updateGame() {
   if (gameMode == GAME_MODE_OFF) {
     clearButtonLED();
+  } else if (gameMode == GAME_MODE_BASIC) {
+    setButtonLED(getRainbowRGB());
   }
 }
--- main/game.h
+++ main/game.h
@@ -8,6 +8,7 @@
 #define GAME_MODE_OFF 0
 #define GAME_MODE_BASIC 1
 
+void changeGameMode(unsigned char newGameMode);
 void updateGame();
 
 #endif
