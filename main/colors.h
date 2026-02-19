da699d84ba331d4c1b25d9f4d23f5720efa6faf0
Calamity-Kitten <Calamity-Kitten@protonmail.com>
2026-02-18 19:09:33 -0500

    Update formatting

--- main/colors.h
+++ main/colors.h
@@ -8,15 +8,15 @@
 #define DEFAULT_RAINBOW_RATE 50
 
 typedef struct {
-  double r;       // a fraction between 0 and 1
-  double g;       // a fraction between 0 and 1
-  double b;       // a fraction between 0 and 1
+  double r; // a fraction between 0 and 1
+  double g; // a fraction between 0 and 1
+  double b; // a fraction between 0 and 1
 } rgb;
 
 typedef struct {
-  double h;       // angle in degrees
-  double s;       // a fraction between 0 and 1
-  double v;       // a fraction between 0 and 1
+  double h; // angle in degrees
+  double s; // a fraction between 0 and 1
+  double v; // a fraction between 0 and 1
 } hsv;
 
 hsv rgb2hsv(rgb in);
